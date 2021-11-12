ad8f83f01ca8d11bef1f3952e9b0e8fb62db4e86
Yuki Nishimura <yukinkopen@gmail.com>
2021-11-13 00:09:11 +0900

    タスク編集／作成画面でタスクの情報を反映・保存させる機能を追加

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -6,21 +6,43 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    let realm = try! Realm()
     var task: Task!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: .modified)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
 
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
     /*
     // MARK: - Navigation
 
