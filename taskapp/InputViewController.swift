99812fce238569c7f891f684a5ff4e8f1406fadf
Yuki Nishimura <yukinkopen@gmail.com>
2021-11-21 14:08:41 +0900

    入力画面のスクロール表示を可能にした

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -9,149 +9,23 @@ import UIKit
 import RealmSwift
 import UserNotifications
 
-class InputViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class InputViewController: UIViewController {
     
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var categoryPicker: UIPickerView!
-    @IBOutlet weak var contentsTextView: UITextView!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var saveButton: UIButton!
-    @IBOutlet weak var cancelButton: UIButton!
-    
-    let realm = try! Realm()
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var containerViewHeight: NSLayoutConstraint!
     
     var task: Task!
-    var categoryArray = try! Realm().objects(Category.self)
-    
-    var isSave = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        self.view.addGestureRecognizer(tapGesture)
+        let inputViewControllerX: InputViewControllerX = self.children[0] as! InputViewControllerX
+        inputViewControllerX.task = task
         
-        categoryPicker.dataSource = self
-        categoryPicker.delegate = self
-        
-        titleTextField.text = task.title
-        if task.category != nil {
-            let categoryIndex = self.categoryArray.index(of: task.category!)
-            if categoryIndex != nil {
-                categoryPicker.selectRow(categoryIndex!, inComponent: 0, animated: false)
-            }
-        }
-        contentsTextView.text = task.contents
-        datePicker.date = task.date
+        inputViewControllerX.initialize()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        categoryArray = try! Realm().objects(Category.self)
-        categoryPicker.reloadAllComponents()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let inputViewControllerX: InputViewControllerX = self.children[0] as! InputViewControllerX
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        if (isSave) {
-            try! realm.write {
-                self.task.title = self.titleTextField.text!
-                self.task.category = categoryArray[self.categoryPicker.selectedRow(inComponent: 0)]
-                self.task.contents = self.contentsTextView.text
-                self.task.date = self.datePicker.date
-                self.realm.add(self.task, update: .modified)
-            }
-            setNotification(task: self.task)
-        }
-        super.viewWillDisappear(animated)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let inputCategoryViewController:InputCategoryViewController = segue.destination as! InputCategoryViewController
-        let category = Category()
-        let allCategories = realm.objects(Category.self)
-        if allCategories.count != 0 {
-            category.id = allCategories.max(ofProperty: "id")! + 1
-        }
-        inputCategoryViewController.category = category
-    }
-    
-    @IBAction func saveBtnAction(_ sender: Any) {
-        isSave = true
-        navigationController?.popViewController(animated: true)
-    }
-    
-    @IBAction func cancelBtnAction(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return categoryArray.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        let cellLabel = UILabel()
-        cellLabel.frame = CGRect(x: 0, y: 0, width: pickerView.rowSize(forComponent: 0).width, height: pickerView.rowSize(forComponent: 0).height)
-                cellLabel.textAlignment = .center
-                cellLabel.font = UIFont.boldSystemFont(ofSize: 16)
-                cellLabel.backgroundColor = UIColor.orange
-                cellLabel.textColor = UIColor.white
-        
-        cellLabel.text = categoryArray[row].name
-                
-        return cellLabel
-    }
-
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-    
-    func setNotification(task: Task) {
-        let content = UNMutableNotificationContent()
-        if task.title == "" {
-            content.title = "(タイトルなし)"
-        } else {
-            content.title = task.title
-        }
-        if task.contents == "" {
-            content.body = "(内容なし)"
-        } else {
-            content.body = task.contents
-        }
-        content.sound = UNNotificationSound.default
-        
-        let calendar = Calendar.current
-        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: String(task.id), content: content, trigger: trigger)
-        
-        let center = UNUserNotificationCenter.current()
-        center.add(request) { (error) in
-            print(error ?? "ローカル通知登録 OK")
-        }
-        
-        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
-            for request in requests {
-                print("/---------------")
-                print(request)
-                print("/---------------")
-            }
-        }
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
